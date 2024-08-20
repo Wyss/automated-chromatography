996558666e9459f0c97610ea3d45e28aa44c528f
David Kalish <davidpkalish@gmail.com>
2024-08-20 13:43:01 -0400

    Added debug (via argparse) to qt-to-py.py
    In both locations

--- SEC_GUI/qt-to-py.py
+++ SEC_GUI/qt-to-py.py
@@ -1,13 +1,23 @@
 # convert *.ui (Qt file) to *.py (PyQt file)
 
 import glob
-# import os
+import argparse
 import subprocess
-# os.chdir(".")
+
+# cmdline argument handling
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--debug",
+        help="Display pyuic5 debug pyuic5 output.",
+        action="store_true")
+args = parser.parse_args()
+
+# do the pyuic5 stuff
 for file in glob.glob("*.ui"):
     print(file)
     name, _ = file.split(".ui")
     cmd = ["pyuic5", "{}.ui".format(name), "-o", "{}.py".format(name), "-x"]
+    if args.debug:
+        cmd.append("-d")
     cmd_str = " ".join(cmd)
     print(cmd_str)
     subprocess.call(cmd_str, shell=True)
